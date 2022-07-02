a96526e161462894709c4688df39b0afbdf2dcd6
Niklas Hiller <niklas.hiller1998@gmail.com>
2022-07-02 13:47:24 +0200

    feat: playback

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from kivy.core.window import Window
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.clock import Clock
+from kivy.animation import Animation, AnimationTransition
+from kivy.uix.slider import Slider
 
 import pyaudio
 import numpy as np
@@ -55,7 +57,9 @@ class MainLayout(FloatLayout):
             pos_hint = {'center_x': 0.5, 'center_y': 0.3},
             always_release = True,
             background_normal = 'assets/microphone_normal.png',
-            background_down = 'assets/microphone_down.png'
+            background_down = 'assets/microphone_down.png',
+            background_disabled_normal = 'assets/microphone_normal.png',
+            border = (0, 0, 0, 0)
         )
         self._recordBtn.bind(on_press = lambda x : self.start_record())
         self._recordBtn.bind(on_release = lambda x : self.stop_record())
@@ -66,11 +70,61 @@ class MainLayout(FloatLayout):
         Clock.schedule_interval(self.update_intensity, 0.0)
         self._recordGlow = Image(
             source = 'assets/microphone_glow.png',
-            size_hint = (None, 0.25 * self.currentGlow),
+            size_hint = (0.0, 0.0),
             pos_hint = {'center_x': 0.5, 'center_y': 0.3}
         )
         self.add_widget(self._recordGlow)
         self.add_widget(self._recordBtn)
+        
+        
+        self._progressSlider = Slider(
+            size_hint = (0.0, 0.0),
+            pos_hint = {'center_x': 0.5, 'center_y': 0.3},
+            value = 0.,
+            opacity = 0.,
+            max = 1.0
+        )
+        self._playBtn = Button(
+            size_hint = (0.0, 0.0),
+            pos_hint = {'right': 0.5, 'center_y': 0.3},
+            always_release = True,
+            background_normal = 'assets/play_normal.png',
+            background_down = 'assets/play_down.png',
+            opacity = 0.,
+            border = (0, 0, 0, 0)
+        )
+        self._pauseBtn = Button(
+            size_hint = (0.0, 0.0),
+            pos_hint = {'right': 0.5, 'center_y': 0.3},
+            always_release = True,
+            background_normal = 'assets/play_normal.png',
+            background_down = 'assets/play_down.png',
+            opacity = 0.,
+            border = (0, 0, 0, 0)
+        )
+        self._confirmBtn = Button(
+            size_hint = (0.0, 0.0),
+            pos_hint = {'center_x': 0.5, 'center_y': 0.3},
+            always_release = True,
+            background_normal = 'assets/confirm_normal.png',
+            background_down = 'assets/confirm_down.png',
+            opacity = 0.,
+            border = (0, 0, 0, 0)
+        )
+        self._cancelBtn = Button(
+            size_hint = (0.0, 0.0),
+            pos_hint = {'center_x': 0.5, 'center_y': 0.3},
+            always_release = True,
+            background_normal = 'assets/cancel_normal.png',
+            background_down = 'assets/cancel_down.png',
+            opacity = 0.,
+            border = (0, 0, 0, 0)
+        )
+        self.add_widget(self._progressSlider)
+        self.add_widget(self._playBtn)
+        self.add_widget(self._pauseBtn)
+        self.add_widget(self._confirmBtn)
+        self.add_widget(self._cancelBtn)
         
         Window.minimum_height = 320
         Window.minimum_width = 640
@@ -88,6 +142,11 @@ class MainLayout(FloatLayout):
             self.currentGlow -= 0.03
         elif self.targetGlow > self.currentGlow:
             self.currentGlow += 0.03
+            
+        if self.currentGlow == 1.0:
+            self._recordGlow.opacity = 0.0
+        else:
+            self._recordGlow.opacity = 1.0
             
         self._recordGlow.size_hint_y = round(0.25 * self.currentGlow, 2)
         self._recordGlow.size_hint_x = round((0.25 * self.currentGlow * Window.height) / Window.width, 2)
@@ -125,6 +184,34 @@ class MainLayout(FloatLayout):
                                         stream_callback = self.process_audio)
         self.stream.start_stream()
     
+    def hide_recordBtn(self, animation, widget):
+        widget.opacity = 0.0
+        anim1 = Animation(size_hint = ((0.15 * Window.height) / Window.width, 0.15),
+                          opacity = 1.0,
+                          pos_hint = {'center_x': 0.3, 'center_y': 0.2},
+                          duration = 0.5, transition = AnimationTransition.in_out_sine)
+        anim2 = Animation(size_hint = ((0.15 * Window.height) / Window.width, 0.15),
+                          opacity = 1.0,
+                          pos_hint = {'center_x': 0.7, 'center_y': 0.2},
+                          duration = 0.5, transition = AnimationTransition.in_out_sine)
+        anim3 = Animation(size_hint = (0.5, 0.05),
+                          opacity = 1.0,
+                          pos_hint = {'center_x': 0.5, 'center_y': 0.35},
+                          duration = 0.5, transition = AnimationTransition.in_out_sine)
+        anim4 = Animation(size_hint = ((0.05 * Window.height) / Window.width, 0.05),
+                          opacity = 1.0,
+                          pos_hint = {'right': 0.245, 'center_y': 0.35},
+                          duration = 0.5, transition = AnimationTransition.in_out_sine)
+        anim5 = Animation(size_hint = ((0.05 * Window.height) / Window.width, 0.05),
+                          opacity = 0.0,
+                          pos_hint = {'right': 0.245, 'center_y': 0.35},
+                          duration = 0.5, transition = AnimationTransition.in_out_sine)
+        anim1.start(self._cancelBtn)
+        anim2.start(self._confirmBtn)
+        anim3.start(self._progressSlider)
+        anim4.start(self._playBtn)
+        anim5.start(self._pauseBtn)
+    
     def stop_record(self):
         # Clock.unschedule(self.glowEvent)
         # self.glowEvent = None
@@ -132,13 +219,17 @@ class MainLayout(FloatLayout):
         self.stream.close()
         self.pyaudio.terminate()
         self.targetGlow = 1.0
+        anim = Animation(size_hint = (0.0, 0.0), duration = 0.5, opacity = 0.0, transition = AnimationTransition.in_out_sine)
+        anim.bind(on_complete = self.hide_recordBtn)
+        anim.start(self._recordBtn)
     
     def update(self, window):
         # Refresh button for recording
         self._recordBtn.size_hint_x = self._recordBtn.height / window.width
-        
-        # Refresh glow effect that indicates dB
-        self._recordGlow.size_hint_x = self._recordGlow.height / window.width
+        # Refresh button for confirm
+        self._confirmBtn.size_hint_x = self._confirmBtn.height / window.width
+        # Refresh button for cancel
+        self._cancelBtn.size_hint_x = self._cancelBtn.height / window.width
         
         # Refresh Text to be recorded
         self._recordText.size = (0.9 * window.width, 0.6 * window.height)
